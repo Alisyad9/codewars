6afea77b33a13065590753297b6ff9b7057f0e5f
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-07-07 19:56:05 +0100

    testing java script

--- test.js
+++ test.js
@@ -1,12 +1,49 @@
 
-function isIsogram(str) {
-    if (str.length===1) {
-      return true 
-    }
-  }
+// function digital_root(n) {
+
+//   let result= n.toString().split('').reduce((a, b) => parseInt(a)+parseInt(b));
+//   if(result>9){
+//     return digital_root(result);
+//   } else 
+//   // console.log(typeof(parseInt(result)));
+//   return parseInt(result);
+//   }
   
-  isIsogram("Dermatoglyphics")// == true
-  isIsogram("aba")// == false
-  isIsogram("moOse") //== false // -- ignore letter case
+ 
+//   digital_root(493193);
   
   //asa
+  // console.log('help');
+
+// function isIsogram(str) {
+//   if (str.length===1) {
+//     return true 
+//   }
+// if(/(\w)\1+/.test(str)){
+//   return false
+
+// }
+
+  //(/([a-zA-Z]).*?\1/).test(str) 
+  
+  // return true
+//   let regex= /([a-zA-Z]).*?\1/
+// if ( regex.test(str)){
+//   return false
+// }
+// return true
+// }
+
+// isIsogram("Dermatoglyphics")// == true
+
+function add(numbers) {
+
+  return numbers.reduce((a,b)=>a+b)
+
+  
+}
+
+console.log( add([2,3,4]) );
+
+add([1,2,3])
+// console.log(add([1,2,3]));
